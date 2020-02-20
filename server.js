d1c5b8520cb67c385af0df700e91e1cfb52f5f89
Igorth <igordiasth@gmail.com>
2020-02-20 19:12:57 +1300

    finish the page

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ server.use(express.static('public'))
 // habilitar body do formulario
 server.use(express.urlencoded({ extended: true}))
 
+// Configurar a conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: '',
+    password: '',
+    host: '',
+    port: 5432,
+    database: 'donation'
+});
+
 // configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -15,30 +25,15 @@ nunjucks.configure("./", {
     noCache: true,
 })
 
-// Lista de doadores: Array
-const donors = [
-    {
-        name: "Igor Dias",
-        blood: "AB+"
-    },
-    {
-        name: "Laisa Dias",
-        blood: "A+"
-    },
-    {
-        name: "Igor Dias",
-        blood: "B+"
-    },
-    {
-        name: "Igor Dias",
-        blood: "O+"
-    }
-]
-
-
 //Configurar a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html", { donors })
+    db.query("SELECT * FROM donors", function(err, result){
+        if(err) return res.send("erro de banco de dados")
+
+        const donors = result.rows;
+        return res.render("index.html", { donors })
+    })
+    
 })
 
 // registrar e guardar dados
@@ -48,14 +43,23 @@ server.post("/", function(req, res){
     const email = req.body.email
     const blood = req.body.blood
 
+    if (name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios.")
+    }
 
-    // colocar valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood,
+
+    // colocar valores dentro do banco de dados
+    const query = `
+        INSERT INTO donors ("name", "email", "blood") 
+        VALUES ($1, $2, $3)`;
+
+    const values = [name, email, blood]
+    
+    db.query(query, values, function(err){
+        if (err) return res.send("erro no banco de dados.")
+        
+        return res.redirect("/")
     })
-
-    return res.redirect("/")
 
 })
 
