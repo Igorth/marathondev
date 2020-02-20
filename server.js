a315a1f2f4c571ce0a558c9ce2e5dca2b71ddadd
Igorth <igordiasth@gmail.com>
2020-02-20 17:56:57 +1300

    html dynamic

--- server.js
+++ server.js
@@ -8,17 +8,38 @@ server.use(express.static('public'))
 // configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true,
 })
+
+// Lista de doadores: Array
+const donors = [
+    {
+        name: "Igor Dias",
+        blood: "AB+"
+    },
+    {
+        name: "Laisa Dias",
+        blood: "A+"
+    },
+    {
+        name: "Igor Dias",
+        blood: "B+"
+    },
+    {
+        name: "Igor Dias",
+        blood: "O+"
+    }
+]
 
 
 //Configurar a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html")
+    return res.render("index.html", { donors })
 })
 
 
 // Ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000, function(){
-    console.log("Iniciei o servidor.")
+    console.log("Start server.")
 })
